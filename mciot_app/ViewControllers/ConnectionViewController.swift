8ffe89749d8f593a90c173e8de084cbfc19d2041
Timo Müller <muellertimomueller@gmail.com>
2020-05-13 23:16:05 +0200

    added logic for bluetooth checking & alerting the user

--- mciot_app/ViewControllers/ConnectionViewController.swift
+++ mciot_app/ViewControllers/ConnectionViewController.swift
@@ -8,26 +8,33 @@
 
 import UIKit
 
-
-import CoreBluetooth
-
 class ConnectionViewController: UIViewController {
-    
-    // Create centralManager
-    var manager: CBCentralManager!
-
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //manager = CBCentralManager(delegate: self, queue: nil)
-
+        
+        
+        // Call the ESenseManager so that it initializes with the app start
+        print(ESenseManager.shared.isBluetoothOn ?? false)
+    }
+    
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        // Start searching for Bluetooth devices
+        // Check if Bluetooth is turned on
+        if(!ESenseManager.shared.isBluetoothOn){
+            // Present an alert to the user to show him that there is no way around this
+            let alert = UIAlertController(title: "Error", message: "Please turn on Bluetooth to use this functionality", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else{
+            // Start a spinner in the top right
+        }
         
         
     }
-
-
+    
+    
 }
 
