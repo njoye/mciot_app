81d1dae39093b19f8340bafed6dc0443353802b5
Timo Müller <muellertimomueller@gmail.com>
2020-05-13 23:16:44 +0200

    added one shared manager for the main functionality of this app

--- mciot_app/ESenseManager.swift
+++ mciot_app/ESenseManager.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+import CoreBluetooth
+
+class ESenseManager: NSObject, CBCentralManagerDelegate {
+    private var centralManager: CBCentralManager!
+    
+    // Defines if bluetooth is turned on or not
+    var isBluetoothOn: Bool!
+    
+    static let shared = ESenseManager()
+    
+    override init(){
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        // Initialize to false
+        isBluetoothOn = (centralManager.state == .poweredOn)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("[INFO] Bluetooth State Changed")
+        if central.state != .poweredOn {
+            print("[INFO] Bluetooth is turned off!")
+            isBluetoothOn = false
+        } else {
+            print("[INFO] Bluetooth is turned ON - Searching for devices now!")
+            isBluetoothOn = true
+            /*centralManager.scanForPeripherals(withServices: [], options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])*/
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
