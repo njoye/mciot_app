79250223e6d75804c04a72a56e6c5b41e02c0240
Timo Müller <muellertimomueller@gmail.com>
2020-05-13 17:47:48 +0200

    Initial Commit

---
+++ mciot_app/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  mciot_app
+//
+//  Created by Timo Müller on 13.05.20.
+//  Copyright © 2020 471. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mciot_app/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  mciot_app
+//
+//  Created by Timo Müller on 13.05.20.
+//  Copyright © 2020 471. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
