708bbdb98e944bff8ddb1cd63722f3c1af9936ed
Timo Müller <muellertimomueller@gmail.com>
2020-05-14 12:26:57 +0200

    added esense pod

--- mciot_app/ESenseManager.swift
+++ mciot_app/ESenseManager.swift
@@ -1,5 +1,5 @@
 //
-//  ESenseManager.swift
+//  MyESenseManager.swift
 //  mciot_app
 //
 //  Created by Timo Müller on 13.05.20.
@@ -9,15 +9,19 @@
 import Foundation
 import UIKit
 import CoreBluetooth
+import ESense
 
-class ESenseManager: NSObject, CBCentralManagerDelegate {
+
+class MyESenseManager: NSObject, CBCentralManagerDelegate {
     
     // The one shared variable that every caller accesses
     // Typical pattern for Swift Apps :)
-    static let shared = ESenseManager()
+    static let shared = MyESenseManager()
     
     // The CoreBluetooth Manager that handles the connections
     private var centralManager: CBCentralManager!
+    private var esenseManager: ESenseManager? = nil
+    
     
     // Defines if bluetooth is turned on or not
     var isBluetoothOn: Bool!
@@ -30,6 +34,15 @@ class ESenseManager: NSObject, CBCentralManagerDelegate {
     private override init(){
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        // Initialize the ESenseManager
+        esenseManager = ESenseManager(deviceName: "eSense-1765", listener: self)
+        
+        // Use this class as sensor listener
+        esenseManager?.registerSensorListener(self, hz: 10)
+        
+        // Use this class as event listener
+        
         
         // Initialize to false
         isBluetoothOn = (centralManager.state == .poweredOn)
@@ -57,9 +70,63 @@ class ESenseManager: NSObject, CBCentralManagerDelegate {
      Starts searching for the desired BLE Device, once it's found, the delegate methods will take over.
      */
     func startSearchingForDevice(){
-        print("[INFO] Starting to search for BLE Device")
+        print("[INFO] Starting to search & connect to BLE Device")
+        print("Connecting: \(esenseManager?.connect(timeout: 10))")
         
         //TODO: add services to look for :)
-        centralManager.scanForPeripherals(withServices: [], options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+        //centralManager.scanForPeripherals(withServices: [], options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+    }
+}
+
+/**
+ The extension that uses the ESense Pod to connect to the Headphones
+ */
+extension MyESenseManager:ESenseConnectionListener{
+    func onDeviceFound(_ manager: ESenseManager) {
+        print("Found device")
+    }
+    
+    func onDeviceNotFound(_ manager: ESenseManager) {
+        print("Not found device")
+    }
+    
+    func onConnected(_ manager: ESenseManager) {
+        print("connected")
+    }
+    
+    func onDisconnected(_ manager: ESenseManager) {
+        print("disconnected")
+    }
+}
+
+extension MyESenseManager:ESenseSensorListener{
+    func onSensorChanged(_ evt: ESenseEvent) {
+        print("Sensor change :) \(evt)")
+    }
+}
+
+extension MyESenseManager:ESenseEventListener{
+    func onBatteryRead(_ voltage: Double) {
+       print("Battery Voltage: \(voltage)")
+    }
+
+    func onButtonEventChanged(_ pressed: Bool) {
+        print("Button value has changed, pressed = \(pressed)")
+    }
+
+    func onAdvertisementAndConnectionIntervalRead(_ minAdvertisementInterval: Int, _ maxAdvertisementInterval: Int, _ minConnectionInterval: Int, _ maxConnectionInterval: Int) {
+        // Leave empty :)
+    }
+
+    func onDeviceNameRead(_ deviceName: String) {
+        print("Got the device name: \(deviceName)")
+    }
+
+    func onSensorConfigRead(_ config: ESenseConfig) {
+        print("Sensor config read")
+    }
+
+    func onAccelerometerOffsetRead(_ offsetX: Int, _ offsetY: Int, _ offsetZ: Int) {
+        print("accelerometer offset read")
     }
 }
--- mciot_app/ViewControllers/ConnectionViewController.swift
+++ mciot_app/ViewControllers/ConnectionViewController.swift
@@ -8,24 +8,29 @@
 
 import UIKit
 
+import ESense
+
 class ConnectionViewController: UIViewController {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var manager:ESenseManager? = nil
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         
-        // Call the ESenseManager so that it initializes with the app start
-        print(ESenseManager.shared.isBluetoothOn ?? false)
+        // Call the MyESenseManager so that it initializes with the app start
+        print(MyESenseManager.shared.isBluetoothOn ?? false)
     }
     
     
     @IBAction func searchButtonPressed(_ sender: Any) {
         // Start searching for Bluetooth devices
         // Check if Bluetooth is turned on
-        if(!ESenseManager.shared.isBluetoothOn){
+        if(!MyESenseManager.shared.isBluetoothOn){
             // Present an alert to the user to show him that there is no way around this
             let alert = UIAlertController(title: "Error", message: "Please turn on Bluetooth to use this functionality", preferredStyle: UIAlertController.Style.alert)
             alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
@@ -35,12 +40,11 @@ class ConnectionViewController: UIViewController {
             activityIndicator.startAnimating()
             
             // Also start searching for a device
-            ESenseManager.shared.startSearchingForDevice()
+            MyESenseManager.shared.startSearchingForDevice()
         }
         
         
     }
-    
     
 }
 
